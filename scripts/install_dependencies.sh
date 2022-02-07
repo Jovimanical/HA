55f99f9efbb6848e93a60a8a8a5d8d21db4fa91f
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-02-07 11:21:02 +0100

    commit -3 resolved apache2 permission

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,26 +3,26 @@ if ! [ -x "$(command -v apache2)" ]; then
 
   # Update Package Index
   sudo apt-get update && sudo apt-get upgrade -y
-  sudo apt-get install ca-certificates apt-transport-https software-properties-common -y
+  sudo apt-get install lsb-release ca-certificates apt-transport-https software-properties-common -y
   sudo add-apt-repository ppa:ondrej/php -y
   sudo add-apt-repository ppa:ondrej/apache2 -y
   sudo apt-get update -y
 
   # Install Apache2
-  sudo apt-get install apache2
+  sudo apt-get install apache2 -y
   # Allow to run Apache on boot up
   sudo systemctl enable apache2
   # Adjust Firewall
   sudo ufw allow in "Apache Full"
   sudo a2enmod rewrite
-  sudo a2dismod mpm_worker
-  sudo a2dismod mpm_prefork
+  #sudo a2dismod mpm_worker
+  #sudo a2dismod mpm_prefork
 
   sudo apt-get install wget unzip -y
   # Install PHP
   sudo apt-get install php8.0 libapache2-mod-php8.0 php8.0-mysql php8.0-gd php8.0-xml php8.0-soap php8.0-mbstring php8.0-mysql php8.0-redis php8.0-curl php8.0-cli php8.0-zip php8.0-yaml -y
 
-  #sudo a2enmod php8.0
+  sudo a2enmod php8.0
 
   # Restart Apache Web Server
   sudo systemctl restart apache2
