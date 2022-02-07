7ae239a92a5e9e62c23bada9b76d89b1419c6723
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-02-07 09:57:24 +0100

    Updated .gitignore to ignore .idea

--- api/ha_categories/delete.php
+++ api/ha_categories/delete.php
@@ -5,44 +5,44 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST,GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
+
 include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/config/helper.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/config/database.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/objects/ha_categories.php';
- include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/token/validatetoken.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/token/validatetoken.php';
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
- 
+
 // prepare ha_categories object
 $ha_categories = new Ha_Categories($db);
- 
+
 // get ha_categories id
 $data = json_decode(file_get_contents("php://input"));
- 
+
 // set ha_categories id to be deleted
 $ha_categories->id = $data->id;
- 
+
 // delete the ha_categories
-if($ha_categories->delete()){
- 
+if ($ha_categories->delete()) {
+
     // set response code - 200 ok
     http_response_code(200);
- 
+
     // tell the user
-	echo json_encode(array("status" => "success", "code" => 1,"message"=> "Ha_Categories was deleted","data"=> ""));
-    
-}
- 
-// if unable to delete the ha_categories
-else{
- 
+    echo json_encode(array("status" => "success", "code" => 1, "message" => "Ha_Categories was deleted", "data" => ""));
+
+} // if unable to delete the ha_categories
+else {
+
     // set response code - 400 bad request
     http_response_code(400);
- 
+
     // tell the user
-	echo json_encode(array("status" => "error", "code" => 0,"message"=> "Unable to delete ha_categories.","data"=> ""));
+    echo json_encode(array("status" => "error", "code" => 0, "message" => "Unable to delete ha_categories.", "data" => ""));
 }
 ?>
--- api/ha_categories/read.php
+++ api/ha_categories/read.php
@@ -5,11 +5,11 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/config/database.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/objects/ha_categories.php';
- include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/token/validatetoken.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'api/token/validatetoken.php';
 // instantiate database and ha_categories object
 $database = new Database();
 $db = $database->getConnection();
- 
+
 // initialize object
 $ha_categories = new Ha_Categories($db);
 
@@ -20,58 +20,58 @@ $ha_categories->no_of_records_per_page = isset($_GET['pagesize']) ? $_GET['pages
 // query ha_categories
 $stmt = $ha_categories->read();
 $num = $stmt->rowCount();
- 
+
 // check if more than 0 record found
-if($num>0){
- 
+if ($num > 0) {
+
     //ha_categories array
-    $ha_categories_arr=array();
-	$ha_categories_arr["pageno"]=$ha_categories->pageNo;
-	$ha_categories_arr["pagesize"]=$ha_categories->no_of_records_per_page;
-    $ha_categories_arr["total_count"]=$ha_categories->total_record_count();
-    $ha_categories_arr["records"]=array();
- 
+    $ha_categories_arr = array();
+    $ha_categories_arr["pageno"] = $ha_categories->pageNo;
+    $ha_categories_arr["pagesize"] = $ha_categories->no_of_records_per_page;
+    $ha_categories_arr["total_count"] = $ha_categories->total_record_count();
+    $ha_categories_arr["records"] = array();
+
     // retrieve our table contents
-    
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
- 
-        $ha_categories_item=array(
-            
-"id" => $id,
-"parent_id" => $parent_id,
-"name" => $name,
-"icon" => $icon,
-"meta_title" => html_entity_decode($meta_title),
-"meta_description" => html_entity_decode($meta_description),
-"meta_keywords" => $meta_keywords,
-"image" => html_entity_decode($image),
-"is_top" => $is_top,
-"is_special" => $is_special,
-"in_filter_menu" => $in_filter_menu,
-"created_at" => $created_at,
-"updated_at" => $updated_at,
-"deleted_at" => $deleted_at
+
+        $ha_categories_item = array(
+
+            "id" => $id,
+            "parent_id" => $parent_id,
+            "name" => $name,
+            "icon" => $icon,
+            "meta_title" => html_entity_decode($meta_title),
+            "meta_description" => html_entity_decode($meta_description),
+            "meta_keywords" => $meta_keywords,
+            "image" => html_entity_decode($image),
+            "is_top" => $is_top,
+            "is_special" => $is_special,
+            "in_filter_menu" => $in_filter_menu,
+            "created_at" => $created_at,
+            "updated_at" => $updated_at,
+            "deleted_at" => $deleted_at
         );
- 
+
         array_push($ha_categories_arr["records"], $ha_categories_item);
     }
- 
+
     // set response code - 200 OK
     http_response_code(200);
- 
+
     // show ha_categories data in json format
-	echo json_encode(array("status" => "success", "code" => 1,"message"=> "ha_categories found","data"=> $ha_categories_arr));
-    
-}else{
- // no ha_categories found will be here
+    echo json_encode(array("status" => "success", "code" => 1, "message" => "ha_categories found", "data" => $ha_categories_arr));
+
+} else {
+    // no ha_categories found will be here
 
     // set response code - 404 Not found
     http_response_code(404);
- 
+
     // tell the user no ha_categories found
-	echo json_encode(array("status" => "error", "code" => 0,"message"=> "No ha_categories found.","data"=> ""));
-    
+    echo json_encode(array("status" => "error", "code" => 0, "message" => "No ha_categories found.", "data" => ""));
+
 }
  
 
