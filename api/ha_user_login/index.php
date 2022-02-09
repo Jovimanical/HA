3474f3cd31e5ac8a1bab8c436f2a248235b41b12
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-02-09 16:50:42 +0100

    Updated SMTP credentails

--- api/ha_user_login/index.php
+++ api/ha_user_login/index.php
@@ -2,14 +2,16 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header('Access-Control-Allow-Credentials: true');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // The request is using the POST method
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header('Access-Control-Allow-Credentials: true');
     header("HTTP/1.1 200 OK");
     return;
 }
@@ -40,7 +42,7 @@ $data = json_decode(file_get_contents("php://input"));
 
 $whitelist = array('127.0.0.1', "::1");
 $getInfo = null;
-if (!in_array($USER_IP, $whitelist) && !filter_var($USER_IP, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE )) {
+if (!in_array($USER_IP, $whitelist) && !filter_var($USER_IP, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
     //call api
     $RETURNED_URL = file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $USER_IP);
     //decode json data
@@ -104,7 +106,7 @@ try {
                 unset($users->pageNo);
 
                 $token["data"] = $users;
-                $jwt = JWT::encode($token, SECRET_KEY,ALGORITHM);
+                $jwt = JWT::encode($token, SECRET_KEY, ALGORITHM);
                 $tokenOutput = array(
                     "access_token" => $jwt,
                     "expires_in" => $tokenExp,
@@ -136,10 +138,10 @@ try {
             $ha_user_logins->user_id = $users->id;
             $ha_user_logins->user_ip = $USER_IP;
             $ha_user_logins->city = $getInfo === null ? 'UNKNOWN' : $getInfo->geoplugin_city || 'UNKNOWN';
-            $ha_user_logins->country = $getInfo === null ? 'UNKNOWN' :$getInfo->geoplugin_countryName || 'UNKNOWN';
-            $ha_user_logins->country_code = $getInfo === null ? 'UNKNOWN' :$getInfo->geoplugin_countryCode || 'UNKNOWN';
-            $ha_user_logins->longitude = $getInfo === null ? 'UNKNOWN' :$getInfo->geoplugin_latitude || 'UNKNOWN';
-            $ha_user_logins->latitude = $getInfo === null ? 'UNKNOWN' :$getInfo->geoplugin_longitude || 'UNKNOWN';
+            $ha_user_logins->country = $getInfo === null ? 'UNKNOWN' : $getInfo->geoplugin_countryName || 'UNKNOWN';
+            $ha_user_logins->country_code = $getInfo === null ? 'UNKNOWN' : $getInfo->geoplugin_countryCode || 'UNKNOWN';
+            $ha_user_logins->longitude = $getInfo === null ? 'UNKNOWN' : $getInfo->geoplugin_latitude || 'UNKNOWN';
+            $ha_user_logins->latitude = $getInfo === null ? 'UNKNOWN' : $getInfo->geoplugin_longitude || 'UNKNOWN';
             $ha_user_logins->browser = "browser info: " . $ua['name'] . " " . $ua['version'] . " on " . $ua['platform'] . " reports: <br >" . $ua['userAgent'];
             $ha_user_logins->os = $ua['platform'] || 'Unknown';
             $ha_user_logins->created_at = date('Y-m-d H:m:s');
@@ -148,7 +150,7 @@ try {
 
         } else {
             http_response_code(400);
-            echo json_encode(array("status" => "error", "code" => 0, "message" => "Invalid login information, please try again.", "data" =>null));
+            echo json_encode(array("status" => "error", "code" => 0, "message" => "Invalid login information, please try again.", "data" => null));
         }
     } else {
 
--- api/ha_users/create.php
+++ api/ha_users/create.php
@@ -2,9 +2,19 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST,GET, OPTIONS");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header('Access-Control-Allow-Credentials: true');
+    header("HTTP/1.1 200 OK");
+    return;
+}
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
